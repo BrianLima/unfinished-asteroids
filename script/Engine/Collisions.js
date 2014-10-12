14629e35ba35963799027960d62e482f07685201
Brian Lima <brian.ghwt@gmail.com>
2014-10-12 12:24:51 -0300

    Finished day three

--- script/Engine/Collisions.js
+++ script/Engine/Collisions.js
@@ -7,49 +7,46 @@ ENGINE.Collisions = function(entities) {
 ENGINE.Collisions.prototype = {
 
 
-  step: function(delta) {
+    step: function (delta) {
 
-    for (var i = 0; i < this.entities.children.length; i++) {
+        for (var i = 0; i < this.entities.children.length; i++) {
 
-      var a = this.entities.children[i];
+            var a = this.entities.children[i];
 
-      if (!a.collidable) continue;
+            if (!a.collidable) continue;
 
-      for (var j = i + 1; j < this.entities.children.length; j++) {
+            for (var j = i + 1; j < this.entities.children.length; j++) {
 
-        var b = this.entities.children[j];
+                var b = this.entities.children[j];
 
-        if (!b.collidable) continue;
+                if (!b.collidable) continue;
 
-        if (Utils.distance(a, b) < a.radius + b.radius) {
+                if (Utils.distance(a, b) < a.radius + b.radius) {
 
-          if (a.collision) a.collision(b);
-          if (b.collision) b.collision(a);
+                    if (a.collision) a.collision(b);
+                    if (b.collision) b.collision(a);
 
-          if (this.callback) this.callback(a, b);
+                    if (this.callback) this.callback(a, b);
 
+                }
+
+            }
         }
 
-      }
-    }
+    },
 
-  },
+    step2: function () {
 
-  /* this shit is short but buggy - what a crappy day... at crappy company
-     - temporary leaving it for emergency use */
-
-  step2: function() {
-
-    for (var i = 0; i < this.entities.children.length; i++) {
-      for (var j = 0; j < this.entities.children.length; j++) {
-        var a = this.entities.children[i];
-        var b = this.entities.children[j];
-        if (Utils.distance(a, b) < a.radius + b.radius) {
-          a.collision(b);
-          b.collision(a);
+        for (var i = 0; i < this.entities.children.length; i++) {
+            for (var j = 0; j < this.entities.children.length; j++) {
+                var a = this.entities.children[i];
+                var b = this.entities.children[j];
+                if (Utils.distance(a, b) < a.radius + b.radius) {
+                    a.collision(b);
+                    b.collision(a);
+                }
+            }
         }
-      }
     }
-  }
 
 };
